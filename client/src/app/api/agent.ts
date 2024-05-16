c38874a65bd250ac2e5606155f7b5b807fb958dd
joachim-samonez <joachimsamonez@gmail.com>
2024-05-16 22:48:34 +0800

    Centralized Axios requests

---
+++ client/src/app/api/agent.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000/api/";
+axios.defaults.withCredentials = true;
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: object) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: object) => axios.put(url, body).then(responseBody),
+  del: (url: string) => axios.delete(url).then(responseBody),
+};
+
+const Product = {
+  list: () => requests.get("products"),
+  details: (id: number) => requests.get(`products/${id}`),
+};
+
+const Basket = {
+  get: () => requests.get("basket"),
+  addItem: (productId: number, quantity = 1) =>
+    requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
+  removeItem: (productId: number, quantity = 1) =>
+    requests.del(`basket?productId=${productId}&quantity=${quantity}`),
+};
+
+const agent = {
+  Product,
+  Basket,
+};
+
+export default agent;
--- client/src/features/product/ProductCard.tsx
+++ client/src/features/product/ProductCard.tsx
@@ -4,17 +4,31 @@ import {
   CardActions,
   CardHeader,
   CardMedia,
+  Divider,
   Typography,
 } from "@mui/material";
 import { Product } from "../../app/models/product";
 import { ShoppingCartRounded } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import { LoadingButton } from "@mui/lab";
+import { blue } from "@mui/material/colors";
 
 interface Props {
   product: Product;
 }
 
 export default function ProductCard({ product }: Props) {
+  const [loading, setLoading] = useState(false);
+
+  function handleAddItem(productId: number) {
+    setLoading(true);
+    agent.Basket.addItem(productId)
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
+
   return (
     <Card>
       <CardActionArea component={Link} to={`/product/${product.id}`}>
@@ -31,9 +45,19 @@ export default function ProductCard({ product }: Props) {
           title={product.name}
         />
       </CardActionArea>
-      <CardActions sx={{ justifyContent: "space-between" }}>
-        <ShoppingCartRounded />
-        <Typography variant="h6">₱{product.price}</Typography>
+      <Divider />
+      <CardActions>
+        <LoadingButton
+          loading={loading}
+          onClick={() => handleAddItem(product.id)}
+          size="small"
+          sx={{ mr: "100px" }}
+        >
+          <ShoppingCartRounded />
+        </LoadingButton>
+        <Typography sx={{ color: blue[700] }} variant="h6">
+          ₱{product.price}
+        </Typography>
       </CardActions>
     </Card>
   );
